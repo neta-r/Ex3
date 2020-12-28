d5527c0de0a246cba9626a85e1c5371427478ccd
Neta <neta2611@walla.com>
2020-12-28 17:25:03 +0200

    jkl

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,10 +1,11 @@
 from GraphInterface import GraphInterface
 from Node import Node
 
+
 class DiGraph(GraphInterface):
 
     def __init__(self):
-        self.nodes = {}
+        self.nodes = {int: Node}
         self.num_of_ed = 0
         self.mode_count = 0
 
@@ -18,41 +19,56 @@ class DiGraph(GraphInterface):
         return self.mode_count
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        if id1 not in self.nodes or id2 not in self.nodes or id1==id2:
+        if id1 not in self.nodes or id2 not in self.nodes or id1 == id2:
             return False
-
-        self.mode_count = self.mode_count+1
-        # חסר פה בדיקה אם הצלע קיימת וחיבור שלה
-        # להשלים!!!!!!!!!!!!!!!!!!!!!!
-
+        if id2 in Node.get_ni(self.nodes[id1]).keys():
+            return False
+        Node.add_ni(self.nodes[id1], id2, weight)
+        self.mode_count = self.mode_count + 1
+        self.num_of_ed = self.num_of_ed+1
+        return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
         if node_id in self.nodes:
             return False
         self.nodes[node_id] = Node(key=node_id, pos=pos)
-        self.mode_count = self.mode_count+1
+        self.mode_count = self.mode_count + 1
         return True
 
     def remove_node(self, node_id: int) -> bool:
         if node_id not in self.nodes:
-            return False # שלחתי הודעה בפורום לבדוק מה נסגר עם זה
-        # פה אמורה לבוא שורה שמעיפים את כל השכנים של הקודקוד
+            return False
+        while Node.get_c_tome(self.nodes[node_id]).__len__() != 0:
+            src = list(Node.get_c_tome(self.nodes[node_id]).keys())[0]
+            self.remove_edge(src, node_id)
+        while Node.get_ni(self.nodes[node_id]).__len__() != 0:
+            dst = list(Node.get_ni(self.nodes[node_id]).keys())[0]
+            self.remove_edge(node_id,dst)
         del self.nodes[node_id]
-        self.mode_count = self.mode_count+1
+        self.mode_count = self.mode_count + 1
         return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
         if node_id1 not in self.nodes or node_id2 not in self.nodes or node_id1 == node_id2:
             return False
-        # חסר פה בדיקה אם הצלע קיימת וניתוק שלה
-        # להשלים!!!!!!!!!!!!!!!!!!!!!!
-        self.mode_count = self.mode_count+1
+        if node_id2 in Node.get_ni(self.nodes[node_id1]).keys():
+            return False
+        Node.remove_ni(self.nodes[node_id1], node_id1)
+        Node.remove_ni(self.nodes[node_id1], node_id1)
+        self.mode_count = self.mode_count + 1
+        self.num_of_ed= self.num_of_ed-1
+        return True
+
+    def get_node(self, id_num):
+        return self.nodes[id_num]
 
     def get_all_v(self) -> dict:
         return self.nodes
 
     def all_in_edges_of_node(self, id1: int) -> dict:
-        pass
+        return Node.get_ni(self.nodes[id])
 
     def all_out_edges_of_node(self, id1: int) -> dict:
         pass
+
+# להחליף לגרף מכוון
--- src/Node.py
+++ src/Node.py
@@ -2,30 +2,40 @@ class Node:
     random_key = 0
 
     def __init__(self, key: int = random_key, pos: tuple = None):
-        self.random_key = self.random_key+1
-        self.key = key
-        self.nei = {}
-        self.pos = pos
+        self.random_key = self.random_key + 1
+        self.__key = key
+        self.__nei = {}
+        self.__c_to_me = {}
+        self.__pos = pos
 
     def get_key(self) -> int:
-        return self.key
+        return self.__key
 
-    def get_ni(self) -> dict:
-        return self.nei
-
-    def add_ni(self, other_key: int, weight: float):  # נצטרך בחיבור של שני הקודקודים לשלוח פעמיים לפונקציה הזאת
-        if type(other_key) != int:
-            return
-        if other_key not in self.nei: # להסביר לטל למה עשיתי ככה
-            self.nei[other_key] = weight
-
-    def remove_ni(self, other_key: int): # נצטרך בחיבור של שני הקודקודים לשלוח פעמיים לפונקציה הזאת
-        if type(other_key) != int:
-            return
-        if other_key in self.nei:
-            del self.nei[other_key]
+    def get_pos(self) -> [tuple]:
+        return self.__pos
 
     def get_ni(self):
-        return self.nei.keys()
+        return self.__nei
 
+    def get_c_tome(self):
+        return self.__c_to_me
 
+    def add_ni(self, other_node, weight: float):
+        if type(other_node) != Node:
+            return
+        if Node.get_key(other_node) not in self.__nei:  # להסביר לטל למה עשיתי ככה
+            self.__nei[other_node] = weight
+            Node.add_to_me(other_node, self.get_key(), weight)
+
+    def add_to_me(self, other_key: int, weight: float):
+        self.__c_to_me[other_key] = weight
+
+    def remove_ni(self, other_node):
+        if type(other_node) != Node:
+            return
+        if Node.get_key(other_node) in self.__nei:
+            del self.__nei[other_node]
+            Node.del_from_me(other_node, self.get_key())
+
+    def del_from_me(self, other_key: int):
+        del self.__c_to_me[other_key]
